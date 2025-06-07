d571cf9a14a2b044a1603dff9ef435de542f0d80
Shevik11 <maksshevchuk621@gmail.com>
2025-06-07 23:02:17 +0300

    blet

--- backend/models/models.py
+++ backend/models/models.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import declarative_base
 Base = declarative_base()
 
 class Days(Base):
-    __tablename__ = "days_3"
+    __tablename__ = "schedule"
 
     name_group = Column(String)
     number_of_subgroup = Column(Integer)
